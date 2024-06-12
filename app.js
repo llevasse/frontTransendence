805be154712fb72c58d5908e02bb96911e73256d
Leo <leonardlevasseur@yahoo.com>
2024-06-12 14:47:08 +0200

    start white mode integration

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ async function changeToLogin(){
 	
 	contain.innerHTML=txt;
 }
+
+async function changeToHome(){
+	const contain = document.getElementById("container");
+	const response = await fetch("bodyLessHome.html");
+	const txt = await response.text();
+	
+	contain.innerHTML=txt;
+}
+
+async function changeToWhiteMode(){
+	const style = document.getElementById("style");
+	style.setAttribute('href', "whiteMode.css");
+}
